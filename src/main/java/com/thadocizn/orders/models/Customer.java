dc877f7744ac8319f05023c5d5cc5d13745ad43b
ThaDocIzN <rodan72@msn.com>
2019-02-20 08:48:23 -0600

    added customer class

---
+++ src/main/java/com/thadocizn/orders/models/Customer.java
@@ -0,0 +1,41 @@
+package com.thadocizn.orders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long custCode;
+    @Column(nullable = false)
+    private String custName;
+    @Column
+    private String custCity;
+    @Column
+    private String workingArea;
+    @Column
+    private String custCountry;
+    @Column
+    private String grade;
+    @Column
+    private double openingAmt;
+    @Column
+    private double receiveAmt;
+    @Column
+    private double paymentAmt;
+    @Column
+    private double outstandingAmt;
+    @Column
+    private String phone;
+    @ManyToOne
+    @JoinColumn(name = "agentCode")
+    @JsonIgnore
+    private long agentCode;
+
+    public Customer() {
+    }
+}
