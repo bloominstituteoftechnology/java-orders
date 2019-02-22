f3e48b91dff66353f5e35e6854992f25d1e0758d
johnoro <officialjohnorourke@gmail.com>
2019-02-22 08:39:17 -0500

    Add GET route for customer/order

--- src/main/java/com/lambdaschool/orders/OrdersController.java
+++ src/main/java/com/lambdaschool/orders/OrdersController.java
@@ -2,6 +2,7 @@ package com.lambdaschool.orders;
 
 import com.lambdaschool.orders.models.Agent;
 import com.lambdaschool.orders.models.Customer;
+import com.lambdaschool.orders.models.Order;
 import com.lambdaschool.orders.repositories.AgentRepository;
 import com.lambdaschool.orders.repositories.CustomerRepository;
 import com.lambdaschool.orders.repositories.OrderRepository;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -27,16 +29,33 @@ public class OrdersController {
   @Autowired
   OrderRepository orderRepository;
 
+  @GetMapping("customer/order")
+  public List<Customer> allCustomers() {
+    List<Customer> customers = customerRepository.findAll();
+
+    customers = customers.stream()
+      .map(customer -> {
+        Set<Order> orders = new HashSet<>(
+          orderRepository.customerOrders()
+        );
+        customer.setOrders(orders);
+        return customer;
+      })
+      .collect(Collectors.toList());
+
+    return customers;
+  }
+
   @GetMapping("agents")
   public List<Agent> allAgents() {
     List<Agent> agents = agentRepository.findAll();
 
     agents = agents.stream()
       .map(agent -> {
-        Set<Customer> customers = customerRepository
+        Set<Customer> customers = new HashSet<>(
+          customerRepository
           .agentCustomers()
-          .stream()
-          .collect(Collectors.toSet());
+        );
         agent.setCustomers(customers);
         return agent;
       })
--- src/main/java/com/lambdaschool/orders/repositories/OrderRepository.java
+++ src/main/java/com/lambdaschool/orders/repositories/OrderRepository.java
@@ -2,5 +2,16 @@ package com.lambdaschool.orders.repositories;
 
 import com.lambdaschool.orders.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+  @Query(
+    value = "SELECT o.*" +
+      " FROM orders o, customers c" +
+      " WHERE c.custcode = o.custcode",
+    nativeQuery = true
+  )
+  List<Order> customerOrders();
+}
