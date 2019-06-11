d287d9135db533692a9749f37f241fff3809fac9
Jake Esveld <thejakeesveld@gmail.com>
2019-06-11 13:10:37 -0700

    Add Models

--- src/main/kotlin/com/jakeesveld/orders/Models.kt
+++ src/main/kotlin/com/jakeesveld/orders/Models.kt
@@ -1,2 +1,53 @@
 package com.jakeesveld.orders
 
+import javax.persistence.*
+
+@Entity
+data class Agent @JvmOverloads constructor(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val agentCode: Long,
+        @Column(nullable = false)
+        var name: String? = "",
+        var workingArea: String? = "",
+        var commission: Double? = 0.0,
+        var phone: String? = "",
+        var country: String? = ""
+)
+
+@Entity
+data class Customer @JvmOverloads constructor(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val custCode: Long,
+        @Column(nullable = false)
+        var name: String? = "",
+        var city: String? = "",
+        var workingArea: String? = "",
+        var country: String? = "",
+        var grade: String? = "",
+        var openingAmt: Double? = 0.0,
+        var receiveAmt: Double? = 0.0,
+        var paymentAmt: Double? = 0.0,
+        var outstandingAmt: Double? = 0.0,
+        var phone: String? = "",
+        @ManyToOne(cascade = [CascadeType.ALL])
+        @JoinColumn(name = "agentCode")
+        var agentCode: Long
+)
+
+@Entity
+data class Order @JvmOverloads constructor(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val orderNum: Long,
+        var orderAmt: Double? = 0.0,
+        var advanceAmt: Double? = 0.0,
+        @ManyToOne(cascade = [CascadeType.ALL])
+        @JoinColumn(name = "custCode")
+        val custCode: Long,
+        @ManyToOne(cascade = [CascadeType.ALL])
+        @JoinColumn(name = "agentCode")
+        val agentCode: Long,
+        var orderDesc: String? = ""
+)
