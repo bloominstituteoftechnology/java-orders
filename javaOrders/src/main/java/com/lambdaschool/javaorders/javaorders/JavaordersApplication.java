6462fb4709299b9195f67455274fd539c9af9b61
Greg Johnston <GCJohnston91@gmail.com>
2019-09-11 16:53:31 -0400

    Init

---
+++ javaOrders/src/main/java/com/lambdaschool/javaorders/javaorders/JavaordersApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.javaorders.javaorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaordersApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavaordersApplication.class, args);
+    }
+
+}
