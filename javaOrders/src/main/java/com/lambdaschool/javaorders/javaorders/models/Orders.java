acd2e06de7eb10ca0b06501d14fa4ebca8b6dc95
Greg Johnston <GCJohnston91@gmail.com>
2019-09-11 18:36:29 -0400

    Orders done

--- javaOrders/src/main/java/com/lambdaschool/javaorders/javaorders/models/Orders.java
+++ javaOrders/src/main/java/com/lambdaschool/javaorders/javaorders/models/Orders.java
@@ -1,6 +1,10 @@
 package com.lambdaschool.javaorders.javaorders.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -14,4 +18,82 @@ public class Orders
     private double advanceAmount;
     private String ordDescription;
     private long custCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "custCode")
+    @JsonIgnoreProperties("orders")
+    private List<Customers> customers = new ArrayList<>();
+
+    public Orders()
+    {
+    }
+
+    public Orders(double ordAmount, double advanceAmount, String ordDescription, long custCode, List<Customers> customers)
+    {
+        this.ordAmount = ordAmount;
+        this.advanceAmount = advanceAmount;
+        this.ordDescription = ordDescription;
+        this.custCode = custCode;
+        this.customers = customers;
+    }
+
+    public long getOrderNum()
+    {
+        return orderNum;
+    }
+
+    public void setOrderNum(long orderNum)
+    {
+        this.orderNum = orderNum;
+    }
+
+    public double getOrdAmount()
+    {
+        return ordAmount;
+    }
+
+    public void setOrdAmount(double ordAmount)
+    {
+        this.ordAmount = ordAmount;
+    }
+
+    public double getAdvanceAmount()
+    {
+        return advanceAmount;
+    }
+
+    public void setAdvanceAmount(double advanceAmount)
+    {
+        this.advanceAmount = advanceAmount;
+    }
+
+    public String getOrdDescription()
+    {
+        return ordDescription;
+    }
+
+    public void setOrdDescription(String ordDescription)
+    {
+        this.ordDescription = ordDescription;
+    }
+
+    public long getCustCode()
+    {
+        return custCode;
+    }
+
+    public void setCustCode(long custCode)
+    {
+        this.custCode = custCode;
+    }
+
+    public List<Customers> getCustomers()
+    {
+        return customers;
+    }
+
+    public void setCustomers(List<Customers> customers)
+    {
+        this.customers = customers;
+    }
 }
