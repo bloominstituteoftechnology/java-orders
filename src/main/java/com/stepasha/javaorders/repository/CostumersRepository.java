93f44260876235f5e8b8d3b395ccc3bb8d1804c3
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-06 16:58:12 -0700

    CustomerService
    complete customer service

--- src/main/java/com/stepasha/javaorders/repository/CostumersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stepasha.javaorders.repository;
-
-import com.stepasha.javaorders.models.Customers;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CostumersRepository extends CrudRepository<Customers, Long> {
-}
--- src/main/java/com/stepasha/javaorders/services/CustomerServiceImpl.java
+++ src/main/java/com/stepasha/javaorders/services/CustomerServiceImpl.java
@@ -1,4 +1,33 @@
 package com.stepasha.javaorders.services;
 
-public class CustomerServiceImpl {
+import com.stepasha.javaorders.models.Customers;
+import com.stepasha.javaorders.repository.CustomersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+@Service(value = "customerService")
+public class CustomerServiceImpl implements CustomerService{
+
+    @Autowired
+    private CustomersRepository custRepo;
+
+    @Override
+    public List<Customers> findAll() {
+        List<Customers> list = new ArrayList<>();
+        custRepo.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+    @Override
+    public Customers findCustomerById(long id) {
+        return custRepo.findById(id).orElseThrow(() -> new EntityNotFoundException(Long.toString(id)));
+    }
+
+    @Override
+    public List<Customers> findCustomerByNameLike(String thename) {
+        return custRepo.findByNameContainingIgnoringCase(thename);
+    }
 }
