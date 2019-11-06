6fb86bc67a32600fa28b62f0865009f73e864a96
Patrick Chow <PatrickChow0803@gmail.com>
2019-11-06 16:06:52 -0500

    Initial Commit

---
+++ src/main/java/local/patrickchow0803/patrickchow0803/Patrickchow0803Application.java
@@ -0,0 +1,13 @@
+package local.patrickchow0803.patrickchow0803;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Patrickchow0803Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Patrickchow0803Application.class, args);
+    }
+
+}
---
+++ src/test/java/local/patrickchow0803/patrickchow0803/Patrickchow0803ApplicationTests.java
@@ -0,0 +1,13 @@
+package local.patrickchow0803.patrickchow0803;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Patrickchow0803ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
