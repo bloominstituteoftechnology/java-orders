7a88ebc0b942e482b73fdbad1f17e273999b123a
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-08 11:31:59 -0400

    Agents getters and setters

--- javaorders/src/main/java/tech/gebel/javaorders/models/Agents.java
+++ javaorders/src/main/java/tech/gebel/javaorders/models/Agents.java
@@ -31,4 +31,52 @@ public class Agents {
         this.phone = phone;
         this.workingArea = workingArea;
     }
+
+    public void setAgentCode(long agentCode) {
+        this.agentCode = agentCode;
+    }
+
+    public long getAgentCode() {
+        return agentCode;
+    }
+
+    public String getAgentName() {
+        return agentName;
+    }
+
+    public void setAgentName(String agentName) {
+        this.agentName = agentName;
+    }
+
+    public double getCommission() {
+        return commission;
+    }
+
+    public void setCommission(double commission) {
+        this.commission = commission;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWorkingArea() {
+        return workingArea;
+    }
+
+    public void setWorkingArea(String workingArea) {
+        this.workingArea = workingArea;
+    }
 }
