d8a2be53ee1d153c153216bc5e25af8cc5815cc5
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-08 10:11:31 -0400

    Create project

---
+++ javaorders/src/main/java/tech/gebel/javaorders/JavaordersApplication.java
@@ -0,0 +1,13 @@
+package tech.gebel.javaorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaordersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaordersApplication.class, args);
+    }
+
+}
