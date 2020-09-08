5d22bad88dcf6cb066f31a8a856293bc65536517
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-08 12:40:18 -0400

    Create Customers class

---
+++ javaorders/src/main/java/tech/gebel/javaorders/models/Customers.java
@@ -0,0 +1,39 @@
+package tech.gebel.javaorders.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "custcode")
+    private long customerCode;
+
+    @Column(name = "custcity")
+    private String customerCity;
+
+    @Column(name = "custcountry")
+    private String customerCountry;
+
+    @Column(name = "custname",
+            nullable = false)
+    private String customerName;
+
+    @Column(name = "openingamt")
+    private double openingAmount;
+
+    @Column(name = "outstandingamt")
+    private double outstandingAmount;
+
+    @Column(name = "paymentamt")
+    private double paymentAmount;
+
+    @Column(name = "receiveamt")
+    private double receiveAmount;
+
+    @Column(name = "workingarea")
+    private String workingArea;
+
+    private String grade, phone;
+}
