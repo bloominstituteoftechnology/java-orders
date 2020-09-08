0889fa46c5ffa824695d470b676b85b331c27916
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-08 11:29:11 -0400

    Agents constructors

--- javaorders/src/main/java/tech/gebel/javaorders/models/Agents.java
+++ javaorders/src/main/java/tech/gebel/javaorders/models/Agents.java
@@ -19,4 +19,15 @@ public class Agents {
 
     @Column(name = "workingarea")
     private String workingArea;
+
+    public Agents() {
+    }
+
+    public Agents(String agentName, double commission, String country, String phone, String workingArea) {
+        this.agentName = agentName;
+        this.commission = commission;
+        this.country = country;
+        this.phone = phone;
+        this.workingArea = workingArea;
+    }
 }
