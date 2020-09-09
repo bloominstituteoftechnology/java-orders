d9be5fd8dcad683773276d54b92eb36cc67f35f8
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-08 23:08:02 -0400

    Create Payment class

---
+++ javaorders/src/main/java/tech/gebel/javaorders/models/Payment.java
@@ -0,0 +1,15 @@
+package tech.gebel.javaorders.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "paymentid")
+    private long paymentId;
+
+    @Column(nullable = false, unique = true)
+    private String type;
+}
