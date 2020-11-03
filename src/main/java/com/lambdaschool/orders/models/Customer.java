07e146d7cf435305b546fea9c42283e28c47890c
Simone Ballard <simone.a.ballard@gmail.com>
2020-11-02 21:05:45 -0700

    Customer updated

--- src/main/java/com/lambdaschool/orders/models/Customer.java
+++ src/main/java/com/lambdaschool/orders/models/Customer.java
@@ -1,5 +1,237 @@
 package com.lambdaschool.orders.models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "customers")
 public class Customer
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long customerid;
+
+    @Column(unique = true,
+        nullable = false)
+    private Long custcode;
+
+    private String custname;
+
+    private String custcity;
+
+    private String workingarea;
+
+    private String custcountry;
+
+    private String grade;
+
+    private double openingamt;
+
+    private double receiveamt;
+
+    private double paymentamt;
+
+    private double outstandingamt;
+
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "agentid", nullable = false)
+    private Long agentcode;
+
+    @OneToMany(mappedBy = "customer",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true)
+    List<Order> orders = new ArrayList<>();
+
+    @ManyToMany()
+    @JoinTable(name = "orderpayments",
+        joinColumns = @JoinColumn(name = "orderid"),
+        inverseJoinColumns = @JoinColumn(name = "paymentid"))
+    Set<Payment> payments = new HashSet<>();
+
+    public Customer()
+    {
+    }
+
+    public Customer(
+        Long custcode,
+        String custname,
+        String custcity,
+        String workingarea,
+        String custcountry,
+        String grade,
+        double openingamt,
+        double receiveamt,
+        double paymentamt,
+        double outstandingamt,
+        String phone,
+        Long agentcode)
+    {
+        this.custcode = custcode;
+        this.custname = custname;
+        this.custcity = custcity;
+        this.workingarea = workingarea;
+        this.custcountry = custcountry;
+        this.grade = grade;
+        this.openingamt = openingamt;
+        this.receiveamt = receiveamt;
+        this.paymentamt = paymentamt;
+        this.outstandingamt = outstandingamt;
+        this.phone = phone;
+        this.agentcode = agentcode;
+    }
+
+    public long getCustomerid()
+    {
+        return customerid;
+    }
+
+    public void setCustomerid(long customerid)
+    {
+        this.customerid = customerid;
+    }
+
+    public Long getCustcode()
+    {
+        return custcode;
+    }
+
+    public void setCustcode(Long custcode)
+    {
+        this.custcode = custcode;
+    }
+
+    public String getCustname()
+    {
+        return custname;
+    }
+
+    public void setCustname(String custname)
+    {
+        this.custname = custname;
+    }
+
+    public String getCustcity()
+    {
+        return custcity;
+    }
+
+    public void setCustcity(String custcity)
+    {
+        this.custcity = custcity;
+    }
+
+    public String getWorkingarea()
+    {
+        return workingarea;
+    }
+
+    public void setWorkingarea(String workingarea)
+    {
+        this.workingarea = workingarea;
+    }
+
+    public String getCustcountry()
+    {
+        return custcountry;
+    }
+
+    public void setCustcountry(String custcountry)
+    {
+        this.custcountry = custcountry;
+    }
+
+    public String getGrade()
+    {
+        return grade;
+    }
+
+    public void setGrade(String grade)
+    {
+        this.grade = grade;
+    }
+
+    public double getOpeningamt()
+    {
+        return openingamt;
+    }
+
+    public void setOpeningamt(double openingamt)
+    {
+        this.openingamt = openingamt;
+    }
+
+    public double getReceiveamt()
+    {
+        return receiveamt;
+    }
+
+    public void setReceiveamt(double receiveamt)
+    {
+        this.receiveamt = receiveamt;
+    }
+
+    public double getPaymentamt()
+    {
+        return paymentamt;
+    }
+
+    public void setPaymentamt(double paymentamt)
+    {
+        this.paymentamt = paymentamt;
+    }
+
+    public double getOutstandingamt()
+    {
+        return outstandingamt;
+    }
+
+    public void setOutstandingamt(double outstandingamt)
+    {
+        this.outstandingamt = outstandingamt;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public Long getAgentcode()
+    {
+        return agentcode;
+    }
+
+    public void setAgentcode(Long agentcode)
+    {
+        this.agentcode = agentcode;
+    }
+
+    public List<Order> getOrders()
+    {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders)
+    {
+        this.orders = orders;
+    }
+
+    public Set<Payment> getPayments()
+    {
+        return payments;
+    }
+
+    public void setPayments(Set<Payment> payments)
+    {
+        this.payments = payments;
+    }
 }
