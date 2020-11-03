bee7d1b9c7b1c4df24cf3e1e65f22a8fadf8d8e7
Simone Ballard <simone.a.ballard@gmail.com>
2020-11-02 22:33:47 -0700

    Initial seed data file created, need to resolve errors

---
+++ src/main/java/com/lambdaschool/orders/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.lambdaschool.orders.repositories;
+
+import com.lambdaschool.orders.models.Order;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+}
