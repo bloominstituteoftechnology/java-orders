73ba27704a718d313a01f5af13845fd320b84ce6
aldairbalanzar <abalanzar7@gmail.com>
2020-11-09 13:03:04 -0500

    fixed error in repositories

--- src/main/java/com/aldair/orders/repositories/AgentRepository.java
+++ src/main/java/com/aldair/orders/repositories/AgentRepository.java
@@ -1,8 +1,8 @@
 package com.aldair.orders.repositories;
 
-import com.aldair.orders.models.Customer;
+import com.aldair.orders.models.Agent;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AgentRepository extends CrudRepository<Customer, Long> {
+public interface AgentRepository extends CrudRepository<Agent, Long> {
 
 }
--- src/main/java/com/aldair/orders/repositories/CustomerRepository.java
+++ src/main/java/com/aldair/orders/repositories/CustomerRepository.java
@@ -1,8 +1,8 @@
 package com.aldair.orders.repositories;
 
-import com.aldair.orders.models.Agent;
+import com.aldair.orders.models.Customer;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CustomerRepository extends CrudRepository<Agent, Long> {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
 }
