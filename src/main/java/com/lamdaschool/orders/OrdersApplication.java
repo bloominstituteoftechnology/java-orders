2d2b2755ca699c8e836c4de311c9eedffca02b22
Simone Ballard <simone.a.ballard@gmail.com>
2020-12-07 13:35:44 -0700

    configuration package added

---
+++ src/main/java/com/lamdaschool/orders/OrdersApplication.java
@@ -0,0 +1,16 @@
+package com.lamdaschool.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrdersApplication.class,
+            args);
+    }
+
+}
---
+++ src/test/java/com/lamdaschool/orders/OrdersApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lamdaschool.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
