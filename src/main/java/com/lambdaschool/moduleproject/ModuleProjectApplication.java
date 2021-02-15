65f2dd0174327f7cc3f6dc470cafbcd91b9f8f60
justin-mavity <justinmavity1021@gmail.com>
2021-02-15 15:09:08 -0600

    initial commit

---
+++ src/main/java/com/lambdaschool/moduleproject/ModuleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.moduleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleProjectApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (ModuleProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lambdaschool/moduleproject/ModuleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.moduleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleProjectApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
